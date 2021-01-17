53fde40bf96a84313c329d0036fda691de40ed8a
NormanPen <73986750+NormanPen@users.noreply.github.com>
2021-01-17 22:13:14 +0100

    Projektdateien hinzufügen.

---
+++ RechnerGleitkommazahl/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RechnerGleitkommazahl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
+            Console.WriteLine("Bitte gib den ersten Summanden ein: ");
+            string ersterSummand = Console.ReadLine();
+            Console.WriteLine("Bitte gib einen zweiten Summanden ein: ");
+            string zweiterSummand = Console.ReadLine();
+
+            // Wandel Text in Ganzzahl
+            int ersterSummandAlsZahl = Convert.ToInt32(ersterSummand);
+            int zweiterSummandAlsZahl = Convert.ToInt32(zweiterSummand);
+
+            //Berechnung ausführen
+            int summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+
+            // Ausgabe
+            Console.WriteLine("Die Summe ist: {0}", summe);
+        }
+    }
+}
